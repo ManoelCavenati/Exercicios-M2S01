e50bb19d8c0509ac55c73f53ab7edf357cae56b2
ManoelCavenati <140075906+ManoelCavenati@users.noreply.github.com>
2024-03-23 12:52:36 -0300

    exercicios ex02

---
+++ exercicios/ex02/Main.java
@@ -0,0 +1,18 @@
+package exercicios.ex02;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char nome = 77;
+        int idade = 42;
+        double altura = 1.81D;
+
+        System.out.println("Imprimir:" + nome + " " + idade + " " + altura + "cm");
+
+        System.out.println("Primeira letra do meu nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Minha altura: " + altura + "cm");
+
+    }
+}
